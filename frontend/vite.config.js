ba632b75179f9fb034b27d97ef0080e090608042
korezawa1005 <t.k.oshihiki@gmail.com>
2025-06-09 17:45:09 +0900

    追加

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: '0.0.0.0',   // Docker 内からもアクセス可
     port: 8000,        // コンテナ内 3000 → ホスト 8000 で公開
