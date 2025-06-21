36df7bad78d99b0f8787678f1af5a0ba4e74f62e
korezawa1005 <t.k.oshihiki@gmail.com>
2025-06-21 17:19:34 +0900

    画像の移動、quizページの作成

---
+++ frontend/src/pages/Quiz.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Quiz = () => {
+  const [quiz, setQuiz] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/v1/quizzes", {
+      params: { category: "牌効率" },
+      withCredentials: true
+    }).then((res) => {
+      setQuiz(res.data);
+    }).catch((err) => {
+      console.error("クイズ取得失敗:", err);
+    });
+  },[]);
+
+  if (!quiz) return <div>読み込み中...</div>;
+
+  const handleTileClick = (tileId) => {
+    navigate("/quiz/answer", { state: { quiz, selectedTileId: tileId } });
+  };
+
+  return (
+    
+    <div className="p-4">
+      <div className="text-lg font-bold">{quiz.category}</div>
+      <div className="text-md mt-2">{quiz.round_info}</div>
+
+      <div className="mt-4 flex items-center gap-2 bg-neutral-200 p-2 rounded-md">
+        <span className="text-black font-medium">ドラ：</span>
+        {quiz.discard_tile_urls.map((url, i) => (
+          <img
+            key={i}
+            src={url}
+            alt={`dora-${i}`}
+            className="w-10 border border-white rounded-sm bg-white"
+          />
+        ))}
+      </div>
+  
+      <div className="mt-4 flex gap-1 bg-neutral-200 p-2 rounded-md">
+        {quiz.hand_tile_urls.map((url, i) => (
+          <img
+            key={i}
+            src={url}
+            alt={`tile-${i}`}
+            className="w-10 border border-white rounded-sm bg-white cursor-pointer"
+            onClick={() => handleTileClick(i)} // tileId = index
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // すべてのJS、JSX、TS、TSXファイルをスキャン
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   future: {
   },
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
