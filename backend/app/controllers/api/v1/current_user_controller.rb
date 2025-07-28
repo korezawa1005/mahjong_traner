72a6dd45153bb13f5e7ca12bf1105fdaf8ae45c1
korezawa1005 <t.k.oshihiki@gmail.com>
2025-07-28 09:00:42 +0900

    ユーザー検索機能の追加

--- backend/app/controllers/api/v1/current_user_controller.rb
+++ backend/app/controllers/api/v1/current_user_controller.rb
@@ -2,9 +2,20 @@ class Api::V1::CurrentUserController < ActionController::Base
   protect_from_forgery with: :null_session
   def show
     if current_user
-      render json: { logged_in: true, user: current_user }
+      render json: {
+        logged_in: true, 
+        user: {
+          id: current_user.id,
+          name: current_user.name,
+          email: current_user.email,
+          role: current_user.role
+        }
+      }
     else
-      render json: { logged_in: false }
+      render json: { 
+        logged_in: false,
+        user: nil
+      }
     end
   end
 end
