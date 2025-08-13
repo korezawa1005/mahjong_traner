307fa83107cda5ef33aac92fd27a518b5d71fd75
korezawa1005 <t.k.oshihiki@gmail.com>
2025-08-13 19:49:49 +0900

    フッターのUI調整

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -17,43 +17,35 @@ const Footer = () => {
 
   return (
     <div className="w-screen fixed bottom-0 left-0 z-30">
-      <footer className="bg-black text-white py-4">
-        <div className="max-w-[700px] px-4 mx-auto flex justify-between text-xs">
+      <footer className="bg-black text-white sm:py-4 lg:py-8">
+        <div className="sm:max-w-[700px] lg:max-w-[1100px] px-4 mx-auto flex justify-between text-xs">
           {/* Home */}
           <Link to="/" className="flex flex-col items-center hover:opacity-80">
-            <FaHome className="text-lg mb-1" />
+            <FaHome className="sm:text-xl lg:text-4xl mb-1" />
             ホーム
           </Link>
 
           {/* マイページ（③の方針：常にリンクでOK） */}
           <Link to="/mypage" className="flex flex-col items-center hover:opacity-80">
-            <FaUser className="text-lg mb-1" />
+            <FaUser className="sm:text-xl lg:text-4xl mb-1" />
             マイページ
           </Link>
 
-          {/*
-          // もし未ログイン時は /login へ飛ばしたいなら、上のLinkを下記ボタンに差し替え
-          <button onClick={goMyPage} className="flex flex-col items-center hover:opacity-80">
-            <FaUser className="text-lg mb-1" />
-            マイページ
-          </button>
-          */}
-
           {/* ログイン */}
           <Link to="/login" className="flex flex-col items-center hover:opacity-80">
-            <FaPen className="text-lg mb-1" />
+            <FaPen className="sm:text-xl lg:text-4xl mb-1" />
             ログイン
           </Link>
 
           {/* ヘルプ */}
           <Link to="/question" className="flex flex-col items-center hover:opacity-80">
-            <FaBook className="text-lg mb-1" />
+            <FaBook className="sm:text-xl lg:text-4xl mb-1" />
             ヘルプ
           </Link>
 
           {/* 問題一覧 */}
           <Link to="/problems" className="flex flex-col items-center hover:opacity-80">
-            <FaBookOpen className="text-lg mb-1" />
+            <FaBookOpen className="sm:text-xl lg:text-4xl mb-1" />
             問題一覧
           </Link>
         </div>
