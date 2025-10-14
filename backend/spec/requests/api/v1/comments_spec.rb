ffd4cd0396a320e20d5577a82d37b0aac8d1f84b
korezawa1005 <t.k.oshihiki@gmail.com>
2025-10-14 19:26:14 +0900

    レビュワー

--- backend/spec/requests/api/v1/comments_spec.rb
+++ backend/spec/requests/api/v1/comments_spec.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'warden/jwt_auth'
 
 RSpec.describe 'Api::V1::Comments', type: :request do
+  def auth_headers_for(user)
+    token, = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil)
+    {
+      'HTTP_AUTHORIZATION' => "Bearer #{token}",
+      'ACCEPT' => 'application/json',
+      'CONTENT_TYPE' => 'application/json'
+    }
+  end
+
   describe 'POST /api/v1/users/:user_id/comments' do
     let(:target_user) { create(:user) }
     let(:quiz_session) { create(:quiz_session, user: target_user) }
@@ -15,12 +25,26 @@ RSpec.describe 'Api::V1::Comments', type: :request do
 
     it 'forbids non-reviewers from creating comments' do
       non_reviewer = create(:user, role: :general)
-      sign_in non_reviewer
+      headers = auth_headers_for(non_reviewer)
 
-      post api_v1_user_comments_path(target_user), params:, as: :json
+      post api_v1_user_comments_path(target_user), params:, headers:, as: :json
 
       expect(response).to have_http_status(:forbidden)
       expect(Comment.count).to eq(0)
+    end
+
+    it 'allows reviewers to create comments' do
+      reviewer = create(:user, role: :reviewer)
+      headers = auth_headers_for(reviewer)
+
+      expect do
+        post api_v1_user_comments_path(target_user), params:, headers:, as: :json
+      end.to change(Comment, :count).by(1)
+
+      expect(response).to have_http_status(:created)
+      body = JSON.parse(response.body)
+      expect(body['content']).to eq('レビュー内容')
+      expect(body['reviewer']['id']).to eq(reviewer.id)
     end
   end
 
@@ -39,9 +63,12 @@ RSpec.describe 'Api::V1::Comments', type: :request do
 
     it 'forbids access for users other than reviewer or target user' do
       outsider = create(:user, role: :general)
-      sign_in outsider
+      headers = auth_headers_for(outsider)
 
-      get api_v1_user_comments_path(target_user), params: { quiz_session_id: quiz_session.id }, as: :json
+      get api_v1_user_comments_path(target_user),
+          params: { quiz_session_id: quiz_session.id },
+          headers: headers,
+          as: :json
 
       expect(response).to have_http_status(:forbidden)
     end
