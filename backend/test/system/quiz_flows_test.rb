e1ccdd2564b571414cdee35231baadaef9521dff
korezawa1005 <t.k.oshihiki@gmail.com>
2025-10-15 01:49:32 +0900

    システムテスト予備

---
+++ backend/test/system/quiz_flows_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class QuizFlowsTest < ApplicationSystemTestCase
+  test "user solves a quiz" do
+    skip "Implement once the quiz UI is testable via system tests"
+  end
+end
