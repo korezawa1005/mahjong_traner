0e4c0d6eeef90ff222d24c4aaf2ecafbc37b91c0
korezawa1005 <t.k.oshihiki@gmail.com>
2025-10-15 18:07:55 +0900

    GAの設定、実装

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,28 +15,31 @@ import Terms from "./pages/Terms";
 import Privacy from "./pages/Privacy";
 import Guide from "./pages/Guide";
 import OAuthCallback from "./pages/OAuthCallback";
+import AnalyticsProvider from "./components/AnalyticsProvider";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/sign_up" element={<SignupForm />} />
-        <Route path="/password/forgot" element={<RequestResetPassword />} />
-        <Route path="/password/reset" element={<ResetPassword />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/quiz/answer" element={<Answer />} />
-        <Route path="/quiz/result" element={<Result />} />
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/users/:id" element={<UserPage />} />
-        <Route path="/quiz/history/:sessionId" element={<QuizHistoryDetail />} />
-        <Route path="/quiz/history/:userId/:sessionId" element={<QuizHistoryDetail />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/guide" element={<Guide />} />
-        <Route path="/oauth/google/callback" element={<OAuthCallback />} />
-      </Routes>
+      <AnalyticsProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/sign_up" element={<SignupForm />} />
+          <Route path="/password/forgot" element={<RequestResetPassword />} />
+          <Route path="/password/reset" element={<ResetPassword />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/quiz/answer" element={<Answer />} />
+          <Route path="/quiz/result" element={<Result />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/users/:id" element={<UserPage />} />
+          <Route path="/quiz/history/:sessionId" element={<QuizHistoryDetail />} />
+          <Route path="/quiz/history/:userId/:sessionId" element={<QuizHistoryDetail />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/guide" element={<Guide />} />
+          <Route path="/oauth/google/callback" element={<OAuthCallback />} />
+        </Routes>
+      </AnalyticsProvider>
     </BrowserRouter>
   );
 };
