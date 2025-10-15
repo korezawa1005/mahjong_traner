695c04b400c6eb3c01cb5b04576ae5731ba8e7de
korezawa1005 <t.k.oshihiki@gmail.com>
2025-10-15 15:32:20 +0900

    微修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,11 +4,15 @@ set -e
 echo "== ENTRYPOINT START =="
 rm -f /app/tmp/pids/server.pid
 
-echo "== RUN MIGRATE =="
-bundle exec rails db:migrate
+if [ "$RAILS_ENV" = "test" ]; then
+  echo "== SKIP MIGRATE/SEED IN TEST ENVIRONMENT =="
+else
+  echo "== RUN MIGRATE =="
+  bundle exec rails db:migrate
 
-echo "== RUN SEED =="
-bundle exec rails db:seed
+  echo "== RUN SEED =="
+  bundle exec rails db:seed
+fi
 
 echo "== STARTING SERVER =="
 exec bundle exec puma -C config/puma.rb
